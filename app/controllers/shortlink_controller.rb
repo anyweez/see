a12383ac4e7daabe25c36ac5d706022949dbb485
Luke Segars <luke@lukesegars.com>
2017-07-10 23:52:51 -0500

    Serverside regex test for valid shortnames. + added docs.

--- app/controllers/shortlink_controller.rb
+++ app/controllers/shortlink_controller.rb
@@ -1,5 +1,10 @@
 class ShortlinkController < ApplicationController
 
+    # GET /:name
+    #
+    # Checks to see if a shortlink exists and redirect if so. If not, render a 'create new
+    # shortlink' view. If the shortlink exists then the redirect occurs immediately and no
+    # interstitial page is displayed.
     def redirect
         name = params[:name]
         link = Shortlink.find_by(name: name)
@@ -12,18 +17,36 @@ class ShortlinkController < ApplicationController
         end
     end
 
+    # POST /:name
+    #
+    # Creates a new shortlink with the specified information; this method expects that that data
+    # is provided via a form.
     def create 
         link = params
                 .require(:shortlink)
                 .permit(:name, :url)
         link[:create_ip] = "1.2.3.4"
 
-        Shortlink.create(link)
-
-        redirect_to manage_path
+        # It shouldn't be possible to create an invalid shortlink name unless something nefarious
+        # is happening. FEED THEM TO THE DOGS.
+        if /^[a-zA-Z0-9]+$/.match(link[:name]) != nil
+            Shortlink.create(link)
+            redirect_to manage_path
+        else
+            redirect_to stahp_path
+        end
     end
 
+    # GET /_
+    #
+    # List all shortlinks and allow users to do some basic managerial stuff (deleting links, for example).
     def manage
         @links = Shortlink.all
+    end
+
+    # GET /_/stahp
+    #
+    # Only for displaying to Ben or Ben-like entities who are trying to deface my masterpiece.
+    def stahp
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,17 @@ Rails.application.routes.draw do
   # Management URL's come first; these are reserved URL's and should be ignored if a user manages
   # to create a shortlink containing '_'.
   get '/_', to: 'shortlink#manage', as: 'manage'
-  post '/_create', to: 'shortlink#create', as: 'create'
+  post '/:name', to: 'shortlink#create', as: 'create'
   delete '/:name', to: 'shortlink#delete', as: 'delete'
 
   # Redirection URL's; core route case for normal users.
   get '/:name', to: 'shortlink#redirect', as: 'redirect', constraints: {
-    name: /[a-zA-Z0-9]/,
+    name: /[a-zA-Z0-9]+/,
   }
 
   # Home URL redirects to management page.
   get '/', to: redirect('/_')
+
+  # Nonsense
+  get '/_/stahp', to: 'shortlink#stahp', as: 'stahp'
 end
