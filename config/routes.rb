4c27d5a2859d7fc1438fabe4a70114d4dc13397b
Luke Segars <luke@lukesegars.com>
2017-07-10 20:58:06 -0500

    Adding default root route (redirect to management page)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Management URL's come first; these are reserved URL's and should be ignored if a user manages
+  # to create a shortlink containing '_'.
   get '/_', to: 'shortlink#manage', as: 'manage'
   post '/_create', to: 'shortlink#create', as: 'create'
-
-  get '/:name', to: 'shortlink#redirect', as: 'redirect'
   delete '/:name', to: 'shortlink#delete', as: 'delete'
+
+  # Redirection URL's; core route case for normal users.
+  get '/:name', to: 'shortlink#redirect', as: 'redirect', constraints: {
+    name: /[a-zA-Z0-9]/,
+  }
+
+  # Home URL redirects to management page.
+  get '/', to: redirect('/_')
 end
